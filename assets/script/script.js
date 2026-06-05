323d7c5099a3776943e5bff13e4e87bf5bdd04c4
Azograph <50628970+Azograph@users.noreply.github.com>
2026-06-05 09:46:18 +0200

    FEAT map interractive + marker iss (index.html)

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,25 @@
 /***************
   MAP LEAFLET
 ***************/
+let map = L.map('map').setView([0, 0], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 30,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+const iss_icon = L.Icon.extend({
+    options: {
+        iconSize:     [50, 50],
+        iconAnchor:   [22, 94],
+    }
+});
+
+let icon = new iss_icon ({
+  iconUrl: './assets/img/international-space-station-icon.png'
+});
+
+let marker = L.marker([0,0], {icon: icon}).addTo(map);
 
 
 /*******************
